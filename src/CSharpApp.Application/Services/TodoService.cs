949fe3cef6c435d6bcd66dafbd20764fae783d35
Thanasis <a.stathoulopoulos@pobuca.com>
2024-08-01 17:30:48 +0300

    Fix bugs & add error handling in ToDoService

--- src/CSharpApp.Application/Services/TodoService.cs
+++ src/CSharpApp.Application/Services/TodoService.cs
@@ -12,20 +12,58 @@ public class TodoService : ITodoService
         _client = client;
 
         var baseUrl = configuration["BaseUrl"] ?? throw new ArgumentException("BaseUrl configuration is missing or empty.");
-        _client.BaseAddress = new Uri(baseUrl!);
+        _client.BaseAddress = new Uri(baseUrl);
+    }
+
+    public async Task<ReadOnlyCollection<TodoRecord>?> GetAllTodos()
+    {
+        try
+        {
+            var todoRecords = await _client.GetAsync($"todos");
+
+            if (todoRecords.IsSuccessStatusCode)
+            {
+                var response = await todoRecords.Content.ReadFromJsonAsync<List<TodoRecord>>();
+
+                if (response == null)
+                    return default;
+
+                return response.AsReadOnly();
+            }
+            else
+            {
+                throw new HttpRequestException($"Request failed with status code {todoRecords.StatusCode} ({todoRecords.ReasonPhrase})");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error fetching todo items");
+            throw new Exception(ex.Message);
+        }
     }
 
     public async Task<TodoRecord?> GetTodoById(int id)
     {
-        var response = await _client.GetFromJsonAsync<TodoRecord>($"todos/{id}");
+        try
+        {
+            var todoRecord = await _client.GetAsync($"todos/{id}");
 
-        return response;
-    }
+            if (todoRecord.IsSuccessStatusCode)
+            {
+                var response = await todoRecord.Content.ReadFromJsonAsync<TodoRecord>();
 
-    public async Task<ReadOnlyCollection<TodoRecord>> GetAllTodos()
-    {
-        var response = await _client.GetFromJsonAsync<List<TodoRecord>>($"todos");
-
-        return response!.AsReadOnly();
+                return response;
+            }
+            else
+            {
+                throw new HttpRequestException($"Request failed with status code {todoRecord.StatusCode} ({todoRecord.ReasonPhrase})");
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error fetching todo with item ID: {ItemId}", id);
+            throw new Exception(ex.Message);
+        }
     }
 }
--- src/CSharpApp.Core/Interfaces/ITodoService.cs
+++ src/CSharpApp.Core/Interfaces/ITodoService.cs
@@ -3,5 +3,5 @@ namespace CSharpApp.Core.Interfaces;
 public interface ITodoService
 {
     Task<TodoRecord?> GetTodoById(int id);
-    Task<ReadOnlyCollection<TodoRecord>> GetAllTodos();
+    Task<ReadOnlyCollection<TodoRecord>?> GetAllTodos();
 }
