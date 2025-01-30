df266ff7e5757e1d31b22d157b33cc3d3ed30bd4
Athanasios Stathoulopoulos <techinpurple@gmail.com>
2025-01-31 00:18:31 +0200

    refactor // Organize categories endpoints into a group

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -9,7 +9,7 @@ builder.Logging.ClearProviders().AddSerilog(logger);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDefaultConfiguration();
-builder.Services.AddHttpConfiguration();   
+builder.Services.AddHttpConfiguration();
 builder.Services.AddProblemDetails();
 builder.Services.AddApiVersioning();
 
@@ -46,28 +46,30 @@ productsGroup.MapPost("/", async (Product product, IProductsService productsServ
     .Produces<Product>(StatusCodes.Status201Created)
     .ProducesValidationProblem();
 
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories", async (ICategoriesService categoriesService) =>
+var categoriesGroup = versionedEndpointRouteBuilder.MapGroup("api/v{version:apiVersion}/categories")
+    .WithTags("Categories")
+    .WithOpenApi()
+    .HasApiVersion(1.0);
+
+categoriesGroup.MapGet("/", async (ICategoriesService categoriesService) =>
 {
     var categories = await categoriesService.GetCategories();
     return categories;
 })
-    .WithName("GetCategories")
-    .HasApiVersion(1.0);
+.WithName("GetCategories");
 
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories/{id}", async (int id, ICategoriesService categoriesService) =>
+categoriesGroup.MapGet("/{id}", async (int id, ICategoriesService categoriesService) =>
 {
     var category = await categoriesService.GetCategoryById(id);
     return category;
 })
-    .WithName("GetCategoryById")
-    .HasApiVersion(1.0);
+.WithName("GetCategoryById");
 
-versionedEndpointRouteBuilder.MapPost("api/v{version:apiVersion}/categories", async (Category category, ICategoriesService categoriesService) =>
+categoriesGroup.MapPost("/", async (Category category, ICategoriesService categoriesService) =>
 {
     var createdCategory = await categoriesService.CreateCategory(category);
-    return Results.Created($"categories/{createdCategory.Id}", createdCategory);
+    return Results.CreatedAtRoute("GetCategoryById", new { id = createdCategory.Id, version = "1.0" }, createdCategory);
 })
-    .WithName("CreateCategory")
-    .HasApiVersion(1.0);
+.WithName("CreateCategory");
 
 app.Run();
